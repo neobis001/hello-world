931e3cbdeedefe981c27adfbbba0d4adb1d685b3
neobis001 <nijobias@gmail.com>
2016-11-16 20:29:50 -0800

    test

--- test - Copy - Copy.py
+++
@@ -1,7 +0,0 @@
-import this
-
-print('this should work as a .py file')
-print("here's another alignment")
-print("This is an edit on test.py")
-print("Here's another line!")
-print("Here's yet another line!")
