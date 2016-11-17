648d76e5e42c4f2daf0870ef5dccb32b6ecfc434
neobis001 <nijobias@gmail.com>
2016-11-16 19:56:52 -0800

    test

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import this
 
 print('this should work as a .py file')
-<<<<<<< HEAD
 print("here's another alignment")
 print("This is an edit on test.py")
-=======
 print("Here's another line!")
->>>>>>> refs/remotes/origin/master
